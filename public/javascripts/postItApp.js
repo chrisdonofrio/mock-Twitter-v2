f6e8f098b35d0f5a459248a9bc4f1e5e6df997bc
Chris Donofrio <cdonofrio20@gmail.com>
2016-06-13 18:32:19 -0400

    binding data, setup scope variables, added bootstrap styling

--- public/javascripts/postItApp.js
+++ public/javascripts/postItApp.js
@@ -1,5 +1,18 @@
 var app = angular.module('postItApp', []);
 
-app.controller('mainController', function(){
+app.controller('mainController', function($scope){
+	// attach 'posts' array to $scope to store created posts
+	$scope.posts = [];
+	// store information on posts being created
+	// i.e. the name of the post's author, the text of the message created by the author, and date posted
+	$scope.newPost = {post_author: '', post_text: '', date_posted: ''};
 
+	// function pushing contents of 'newPost' to 'posts' array
+	$scope.post = function(){
+		// making sure new post has current timestamp
+		$scope.newPost.date_posted = Date.now();
+		$scope.posts.push($scope.newPost);
+		// reset new post
+		$scope.newPost = {post_author: '', post_text: '', date_posted: ''};
+	};
 });
